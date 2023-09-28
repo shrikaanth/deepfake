02a7aa948ad6d646fa62df6bece1bb9acb65edb1
Shrikaanth Shyam sunder <67648522+shrikaanth@users.noreply.github.com>
2023-09-28 14:17:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Dropout, Reshape, Concatenate, LeakyReLU
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+
+# Height and width refer to the size of the image
+# Channels refers to the amount of color channels (red, green, blue)
+
+image_dimensions = {'height':256, 'width':256, 'channels':3}
+
+# Create a Classifier class
+class Classifier:
+    def __init__(self):
+        self.model = 0
+    
+    def predict(self, x):
+        return self.model.predict(x)
+    
+    def fit(self, x, y):
+        return self.model.train_on_batch(x, y)
+    
+    def get_accuracy(self, x, y):
+        return self.model.test_on_batch(x, y)
+    
+    def load(self, path):
+        self.model.load_weights(path)
+
+# Create a MesoNet class using the Classifier
+class Meso4(Classifier):
+    def __init__(self, learning_rate=0.001):
+        self.model = self.init_model()
+        optimizer = Adam(lr=learning_rate)
+        self.model.compile(optimizer=optimizer,
+                           loss='mean_squared_error',
+                           metrics=['accuracy'])
+    
+    def init_model(self): 
+        x = Input(shape=(image_dimensions['height'],
+                         image_dimensions['width'],
+                         image_dimensions['channels']))
+        
+        x1 = Conv2D(8, (3, 3), padding='same', activation='relu')(x)
+        x1 = BatchNormalization()(x1)
+        x1 = MaxPooling2D(pool_size=(2, 2), padding='same')(x1)
+        
+        x2 = Conv2D(8, (5, 5), padding='same', activation='relu')(x1)
+        x2 = BatchNormalization()(x2)
+        x2 = MaxPooling2D(pool_size=(2, 2), padding='same')(x2)
+        
+        x3 = Conv2D(16, (5, 5), padding='same', activation='relu')(x2)
+        x3 = BatchNormalization()(x3)
+        x3 = MaxPooling2D(pool_size=(2, 2), padding='same')(x3)
+        
+        x4 = Conv2D(16, (5, 5), padding='same', activation='relu')(x3)
+        x4 = BatchNormalization()(x4)
+        x4 = MaxPooling2D(pool_size=(4, 4), padding='same')(x4)
+        
+        y = Flatten()(x4)
+        y = Dropout(0.5)(y)
+        y = Dense(16)(y)
+        y = LeakyReLU(alpha=0.1)(y)
+        y = Dropout(0.5)(y)
+        y = Dense(1, activation='sigmoid')(y)
+
+        return Model(inputs=x, outputs=y)
+
+# Instantiate a MesoNet model with pretrained weights
+meso = Meso4()
+meso.load('./weights/Meso4_DF')
+
+# Function to predict real or fake for a specific image
+def predict_image(image_path):
+    img = load_img(image_path, target_size=(256, 256))
+    img = img_to_array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    prediction = meso.predict(img)[0][0]
+    
+    if prediction > 0.5:
+        result = "This image is likely a Real."
+    else:
+        result = "This image appears to be Fake."
+    
+    # Show the image
+    plt.imshow(img[0])
+    plt.title(result)
+    plt.show()
+
+    return result
+
+# Provide the path to the image you want to predict
+image_path = 'data/DeepFake/mm.jpg'  # Replace with your image path
+result = predict_image(image_path)
+print(result)
